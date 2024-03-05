5585253705dca14da7f856c3d8c6ca61b7d78bb5
tarkul02 <70142061+tarkul02@users.noreply.github.com>
2024-03-05 15:33:48 +0700

    fix

--- Models/T_LOG_ADD_STOCK.cs
+++ Models/T_LOG_ADD_STOCK.cs
@@ -8,6 +8,18 @@ namespace PostSap_GR_TR.Class
 {
     public class T_LOG_ADD_STOCK
     {
-
+        public string Batch { get; internal set; }
+        public int EntryQnt { get; internal set; }
+        public string EntryUom { get; internal set; }
+        public object FacNo { get; internal set; }
+        public string Material { get; internal set; }
+        public string StgeLoc { get; internal set; }
+        public string MoveType { get; internal set; }
+        public object Plant { get; internal set; }
+        public string Custid { get; internal set; }
+        public string Kanban { get; internal set; }
+        public DateTime UpdDate { get; internal set; }
+        public DateTime StockDate { get; internal set; }
+        public string DocMat { get; internal set; }
     }
 }
